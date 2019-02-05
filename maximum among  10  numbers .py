93a48f88913719e4a2a12791d268af53b18d9e17
SEEVALAPERIYA <43268966+SEEVALAPERIYA@users.noreply.github.com>
2019-02-05 15:19:25 +0530

    Create maximum among  10  numbers .py

---
+++ maximum among  10  numbers .py
@@ -0,0 +1,6 @@
+1st=[]
+num=int(input('how  many  number:'))
+for  n  in range (num):
+    number=int(input('enter  number'))
+    1st.append(numbers)
+print("maximum  element in the  list is:",max(1st),"\n minimum element in the  list is:",min(1st))    
